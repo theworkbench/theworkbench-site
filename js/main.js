e2dbe68b5a9e0e52664a60366f944d6b3246dc93
Lachlan Hardy <lachlan@lachstock.com.au>
2012-08-15 14:20:33 +1000

    Sorting out colorbox options

--- js/main.js
+++ js/main.js
@@ -2,24 +2,18 @@ $(document).ready(function(){
 	$("#register-basic").colorbox({
 		fixed: true,
 		inline:true,
-		overlayClose: true,
-		escKey: true,
 		transition: "elastic",
 		width:"50%"
 	});
 	$("#register-lite").colorbox({
 		fixed: true,
 		inline:true,
-		overlayClose: true,
-		escKey: true,
 		transition: "elastic",
 		width:"50%"
 	});
 	$("#register-full").colorbox({
 		fixed: true,
 		inline:true,
-		overlayClose: true,
-		escKey: true,
 		transition: "elastic",
 		width:"50%"
 	});
