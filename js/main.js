efb4191b68513a6429333c90cdd90b9d2208fb30
Simon Wright <diversionary@gmail.com>
2012-10-19 11:48:32 -0400

    Site revamp
    Github freakout - resyncing everything.
    Adding in new sections, design, images, styles.
    Still needs polishing, finishing and updates to mobile/tablet styles.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#register-basic").colorbox({
+		inline:true,
+		transition: "elastic"
+	});
+	$("#register-lite").colorbox({
+		inline:true,
+		transition: "elastic"
+	});
+	$("#register-full").colorbox({
+		inline:true,
+		transition: "elastic"
+	});
+});
